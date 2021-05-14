c67654b6d2a7f4c49534dcdeff5fb18c18a55384
douglasbpl <douglas.bpl94@yahoo.com.br>
2021-05-14 19:05:52 -0300

    prática 2 de lógica de programação

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 let idade = 27
 const name = "Douglas"
-const nascimento = true
+const ishuman  = true
 const maiord = "Você é maior de idade"
 const menord = "Você é menor de idade"
 const mes = "Maio"
@@ -12,19 +12,19 @@ if (idade > 18)
 {console.log(`Você tem mais que 18`)} 
 else {console.log(`Você tem 18 ou menos`)}
 
-if (idade >= 18 && nascimento ) {console.log(maiord )}
+if (idade >= 18 && ishuman) {console.log(maiord )}
 else {console.log(menord)}
 
 
-if(mes == "Janeiro" || mes == "Dezembro")
+if(mes === "Janeiro" || mes === "Dezembro")
 {console.log(`${name} faz aniversário`)}
 else {console.log(`Não é seu aniversário`)}
 
 
-if(initialletter == "R") {console.log(`Seu nome começa com R`)}
+if(initialletter === "R") {console.log(`Seu nome começa com R`)}
 else{console.log(`Seu nome não começa com R`)}
 
-if((sobrenome > "6") || (initialletter == "E")) {console.log(` Você foi o escolhido`)}
+if((sobrenome > "6") || (initialletter === "E")) {console.log(` Você foi o escolhido`)}
 else {console.log(`Você não foi o escolhido`)}
 
 
