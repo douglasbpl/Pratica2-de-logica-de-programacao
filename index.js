3abe6a59bd8b5e6c38d8ff0198fb70361d3979b8
douglasbpl <douglas.bpl94@yahoo.com.br>
2021-05-28 15:54:21 -0300

    prática 2 de lógica de programação

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 let idade = 27
 const name = "Douglas"
-const ishuman  = true
+const isHuman  = true
 const maiord = "Você é maior de idade"
 const menord = "Você é menor de idade"
 const mes = "Maio"
+const sobrenome = "Brasil"
 
 
 // 1 - Crie uma condição que exiba uma mensagem no console se a idade for maior que 18
@@ -14,26 +15,27 @@ else {console.log(`Você tem 18 ou menos`)}
 
 // 2 - Crie uma condição que exiba uma mensagem no console se você for maior de idade E a condição humana seja true
 
-if (idade >= 18 && ishuman) {console.log(maiord )}
+if ((idade >= 18) && (isHuman == true))
+{console.log(maiord )}
 else {console.log(menord)}
 
 // 3 - Criar uma condição que exiba uma mensagem no console se você fizer aniversário em Janeiro OU Dezembro
 
-if(mes === "Janeiro" || mes === "Dezembro")
+if((mes === "Janeiro") || (mes === "Dezembro"))
 {console.log(`${name} faz aniversário`)}
 else {console.log(`Não é seu aniversário`)}
 
 // 4 - Criar uma condição que exiba uma mensagem no console se seu nome começar com a letra R
 
 
-if(name.substring(0, 1) === "R") {console.log(`Seu nome começa com R`)}
-else{console.log(`Seu nome não começa com R`)}
+if(name.substring(0, 1) === "R") {console.log(`Seu nome começa com "R"`)}
+else{console.log(`Seu nome não começa com "R"`)}
 
 // 5 - Criar uma condição que exiba uma mensagem no console se seu sobrenome tenha mais de 6 letras OU seu nome começar com a letra E
 
 
-if(name.length > 6 || name.substring(0, 1) === "E")
-{console.log("Seu nome tem mais de 6 letras ou seu nome começa com a letra 'E'")}
+if((sobrenome.length >= 6) || (name.substring(0, 1) === "E"))
+{console.log("Seu sobrenome tem mais de 6 letras ou seu nome começa com a letra 'E'")}
 
 
 
